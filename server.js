1bc063733fa42e9ba51d67afb6f98b33e0130bd5
Rye-Guy <ryanracioppo95@gmail.com>
2018-09-10 13:30:46 -0400

    added connection to mongodb

--- server.js
+++ server.js
@@ -10,11 +10,21 @@ console.log(process.cwd());
 app.use(express.static(__dirname + '/public'));
 
 
+const db = 'mongodb://localhost/nytReactApp';
+mongoose.connect(db, (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log('Mongoose Connection Successful');
+    }
+});
+
 const router = new express.Router();
 
 router.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
 
 const port = process.env.PORT || 2020;
 app.listen(port, ()=>{
