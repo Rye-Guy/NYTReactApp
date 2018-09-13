240b82f4bf8775d4a54c7f47009c762a998ac371
Rye-Guy <ryanracioppo95@gmail.com>
2018-09-13 13:05:34 -0400

    Building for production

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 if(process.env.NODE_ENV === "production"){
-app.use(express.static('client/build/'));
+app.use(express.static('/client/build/'));
 }else{
     app.use(express.static(__dirname, '/client/public'));
 }
